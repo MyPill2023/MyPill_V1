3ebf232f9321e3f57c7477df0a4f508a6f667c34
jny0 <pjy100402@naver.com>
2023-06-21 16:42:14 +0900

    Feat : BaseEntity 추가

---
+++ src/main/java/com/mypill/global/base/entitiy/BaseEntity.java
@@ -0,0 +1,37 @@
+package com.mypill.global.base.entitiy;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@MappedSuperclass
+@Getter
+@SuperBuilder(toBuilder = true)
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+@ToString
+public class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    @CreatedDate
+    private LocalDateTime createDate;
+
+    @LastModifiedDate
+    private LocalDateTime modifyDate;
+
+    private LocalDateTime deleteDate;
+}
