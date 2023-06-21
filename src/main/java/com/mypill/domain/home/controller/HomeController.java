044102e0d2f0b536b0b4d14bab390662b9726b99
jny0 <pjy100402@naver.com>
2023-06-21 16:20:57 +0900

    Feat : 기본 UI 템플릿 추가

---
+++ src/main/java/com/mypill/domain/home/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.mypill.domain.home.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class HomeController {
+    @GetMapping("/")
+    public String showMain() {
+        return "usr/home/main";
+    }
+
+}
