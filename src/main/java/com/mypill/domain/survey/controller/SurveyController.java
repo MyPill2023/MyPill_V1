fff54adb2eef715d720ee45ce3d43b3d4f5ace98
leemimi <mijeong1015@naver.com>
2023-06-21 17:32:31 +0900

    Feat: 설문 기본 베이스 생성

---
+++ src/main/java/com/mypill/domain/survey/controller/SurveyController.java
@@ -0,0 +1,13 @@
+package com.mypill.domain.survey.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/usr/survey")
+public class SurveyController {
+
+
+}
---
+++ src/main/java/com/mypill/domain/survey/service/SurveyService.java
@@ -0,0 +1,11 @@
+package com.mypill.domain.survey.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class SurveyService {
+}
