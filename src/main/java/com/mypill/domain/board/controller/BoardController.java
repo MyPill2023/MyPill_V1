90afcc3f3517dccaa905b29cf7d527c153b18e1b
dw2023 <dwkim126@gmail.com>
2023-06-22 11:06:00 +0900

    Feat : layout.html Q&A게시판 버튼 링크주소 추가

--- src/main/java/com/mypill/domain/board/controller/BoardController.java
+++ src/main/java/com/mypill/domain/board/controller/BoardController.java
@@ -1,11 +1,28 @@
 package com.mypill.domain.board.controller;
 
+import com.mypill.domain.board.entity.Board;
+import com.mypill.domain.board.service.BoardService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-@RequestMapping("/usr")
+import java.util.List;
+
+@RequestMapping("/board")
 @RequiredArgsConstructor
 @Controller
 public class BoardController {
+    private final BoardService boardService;
+
+    @PreAuthorize("isAnonymous()")
+    @GetMapping("/list")
+    public String showList(Model model) {
+        List<Board> boardList = boardService.getList();
+        model.addAttribute("boardList", boardList);
+        return "board/list";
+    }
 }
