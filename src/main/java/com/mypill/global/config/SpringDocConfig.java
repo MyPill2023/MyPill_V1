297d0de07bdd65b41f71616422499deddb3b5644
jny0 <pjy100402@naver.com>
2023-06-23 09:59:35 +0900

    Feat : Swagger 적용

---
+++ src/main/java/com/mypill/global/config/SpringDocConfig.java
@@ -0,0 +1,10 @@
+package com.mypill.global.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(info = @Info(title = "Mypill REST API", version = "v1"))
+public class SpringDocConfig {
+}
