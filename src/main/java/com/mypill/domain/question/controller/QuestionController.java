4faa7405ede3ba1c82677b41fa506339fa28d66a
dw2023 <dwkim126@gmail.com>
2023-06-23 10:25:16 +0900

    Feat : Swagger 관련 어노테이션 추가

--- src/main/java/com/mypill/domain/question/controller/QuestionController.java
+++ src/main/java/com/mypill/domain/question/controller/QuestionController.java
@@ -2,6 +2,8 @@ package com.mypill.domain.question.controller;
 
 import com.mypill.domain.question.entity.Question;
 import com.mypill.domain.question.service.QuestionService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.ui.Model;
@@ -14,11 +16,12 @@ import java.util.List;
 @RequestMapping("/question")
 @RequiredArgsConstructor
 @RestController
+@Tag(name = "QuestionController", description = "질문게시판")
 public class QuestionController {
     private final QuestionService questionService;
 
-    @PreAuthorize("isAnonymous()")
     @GetMapping("/list")
+    @Operation(summary = "질문 목록")
     public String showList(Model model) {
         List<Question> questionList = questionService.getList();
         model.addAttribute("questionList", questionList);
