2ef512689729633268f7b66016b6c5f79f29cd77
jny0 <94813918+jny0@users.noreply.github.com>
2023-06-28 14:51:31 +0900

    Feat : 장바구니 (#37)
    * Feat : ProductRequest List 타입 수정
    
    * Feat : 장바구니 엔티티
    
    * Feat : 장바구니에 상품 추가
    
    * Feat : 장바구니 페이지
    
    * Feat : 이미 장바구니에 담겨있는 상품이라면 수량 업데이트
    
    * Feat : 장바구니에서 수량 수정
    
    * Feat : 장바구니 상품 삭제
    
    * Fix : 장바구니에서 수량 버튼 버그 수정
    
    * Feat : CartResponseDTO 적용
    
    * Test : 장바구니 관련 테스트케이스
    
    * Feat : 총 금액, 총 수량 필드
    
    * Fix : 총 주문수량/주문금액 업데이트 버그 수정
    
    * Refactor : quantity 타입 Long으로 변경

---
+++ src/main/java/com/mypill/domain/cart/dto/request/CartProductRequest.java
@@ -0,0 +1,15 @@
+package com.mypill.domain.cart.dto.request;
+
+import jakarta.validation.constraints.Min;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+
+@Data
+@AllArgsConstructor
+public class CartProductRequest {
+    private Long productId;
+    @Min(value=1, message = "수량은 1개 이상이어야 합니다.")
+    private Long quantity;
+
+}
