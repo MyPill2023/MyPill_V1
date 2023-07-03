07c2e96371b0efd3f0896c62599ed8b20107fb77
jny0 <94813918+jny0@users.noreply.github.com>
2023-07-03 13:03:57 +0900

    Feat : 판매자 - 주문 상태 관리 (#60)
    * Feat : 판매자 주문 리스트 조회
    
    * Feat : 판매자의 주문 상태 관리

--- src/main/java/com/mypill/domain/order/repository/OrderRepository.java
+++ src/main/java/com/mypill/domain/order/repository/OrderRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import java.util.Optional;
 
-public interface OrderRepository extends JpaRepository<Order, Long>{
+public interface OrderRepository extends JpaRepository<Order, Long>, OrderRepositoryCustom {
     List<Order> findByBuyerId(Long buyerId);
 }
