5afc60bc8821cb2eecdf6698041e1c9156cb5ba3
leemimi <mijeong1015@naver.com>
2023-07-10 17:36:30 +0900

    Feat: Diary Service Test Code 추가

---
+++ src/test/java/com/mypill/domain/diary/service/DiaryServiceTest.java
@@ -0,0 +1,74 @@
+package com.mypill.domain.diary.service;
+
+
+import com.mypill.domain.diary.entity.Diary;
+import com.mypill.domain.diary.entity.DiaryCheckLog;
+import com.mypill.domain.diary.repository.DiaryCheckLogRepository;
+import com.mypill.domain.diary.repository.DiaryRepository;
+import com.mypill.domain.member.entity.Member;
+import com.mypill.domain.member.service.MemberService;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+@SpringBootTest
+@Transactional
+@ActiveProfiles("test")
+@TestMethodOrder(MethodOrderer.MethodName.class)
+class DiaryServiceTest {
+
+    @Autowired
+    private MemberService memberService;
+    private Member testUser1;
+    private Diary diary;
+    @Autowired
+    private DiaryRepository diaryRepository;
+    @Autowired
+    private DiaryService diaryService;
+    private Diary savedDiary;
+    @Autowired
+    private DiaryCheckLogRepository diaryCheckLogRepository;
+
+    @BeforeEach
+    void beforeEachTest() {
+        testUser1 = memberService.join("testUserSeller1", "김철수", "1234", 2, "testSeller1@test.com").getData();
+        diary = Diary.builder()
+                .member(testUser1)
+                .name("루테인")
+                .time("morning")
+                .type("간")
+                .memo("x")
+                .build();
+        savedDiary = diaryRepository.save(diary);
+
+        DiaryCheckLog diaryCheckLog = DiaryCheckLog.builder()
+                .diary(savedDiary)
+                .member(testUser1)
+                .checkDate(LocalDate.now())
+                .build();
+        diaryCheckLogRepository.save(diaryCheckLog);
+    }
+
+    @Test
+    @DisplayName("당일 날짜에 해당하는 체크한 기록 불러오기")
+    void findHistoryTests() {
+        LocalDate currentDate = LocalDate.of(2023, 7, 9);
+        List<Diary> history = diaryService.findHistory(testUser1, LocalDate.now());
+        List<Diary> history1 = diaryService.findHistory(testUser1, currentDate);
+
+        assertThat(history1.size()).isEqualTo(0);
+        assertThat(history.size()).isEqualTo(1);
+
+    }
+
+}
+
