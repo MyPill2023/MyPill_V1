5e1abd9291871d030646752f0092e7a43b27f0b2
hojunking96 <hojunking96@gmail.com>
2023-07-19 23:45:46 +0900

    Fix : 테스트 통과 위해 test.yml 다시 수정

--- src/main/java/com/mypill/MypillApplication.java
+++ src/main/java/com/mypill/MypillApplication.java
@@ -3,6 +3,7 @@ package com.mypill;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -10,6 +11,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableJpaAuditing
 @EntityScan
 @EnableScheduling
+@EnableConfigurationProperties
 public class MypillApplication {
     public static void main(String[] args) {
         SpringApplication.run(MypillApplication.class, args);
