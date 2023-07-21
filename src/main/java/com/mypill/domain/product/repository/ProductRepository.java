adc40fb91e18d90ffaa1802db1a1ab56400712da
jny0 <pjy100402@naver.com>
2023-07-22 03:27:23 +0900

    Refactor : 재고처리로직 비관적 락 걸기

--- src/main/java/com/mypill/domain/product/repository/ProductRepository.java
+++ src/main/java/com/mypill/domain/product/repository/ProductRepository.java
@@ -1,8 +1,13 @@
 package com.mypill.domain.product.repository;
 
 import com.mypill.domain.product.entity.Product;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ProductRepository extends JpaRepository<Product, Long>, ProductRepositoryCustom {
-
+    @Lock(value = LockModeType.PESSIMISTIC_WRITE)
+    @Query("select p from Product p where p.id = :id")
+    Product findByWithPessimisticLock(final Long id);
 }
