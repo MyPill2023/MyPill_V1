c0ff27be5635d00766444a57f2a60d3d5fb1dbf8
hojunking96 <hojunking96@gmail.com>
2023-08-27 21:32:49 +0900

    Refactor : Post DTO 수정 마무리

--- src/main/java/com/mypill/domain/member/controller/MemberController.java
+++ src/main/java/com/mypill/domain/member/controller/MemberController.java
@@ -107,7 +107,7 @@ public class MemberController {
     @Operation(summary = "내 게시글 목록 페이지")
     public String showMyPosts(Model model) {
         List<Post> posts = postService.getMyPosts(rq.getMember());
-        model.addAttribute("response", PostsResponse.of(posts));
+        model.addAttribute("postsResponse", PostsResponse.of(posts));
         return "usr/member/myPosts";
     }
 
