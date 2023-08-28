31b4865d3b694e8e1311eab52cb014697f2cfabd
leemimi <mijeong1015@naver.com>
2023-08-28 15:25:15 +0900

    Refactor: Diary DTO 수정

--- src/main/java/com/mypill/domain/diary/dto/response/DiaryCheckListResponse.java
+++ src/main/java/com/mypill/domain/diary/dto/response/DiaryCheckListResponse.java
@@ -2,21 +2,26 @@ package com.mypill.domain.diary.dto.response;
 
 import com.mypill.domain.diary.entity.Diary;
 import com.mypill.domain.diary.entity.DiaryCheckLog;
-import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Map;
 
+
 @Data
-@AllArgsConstructor
+@Builder
 public class DiaryCheckListResponse {
-    private final String today;
-    private final List<Diary> diaries;
-    private final Map<LocalDate, List<DiaryCheckLog>> groupedData;
+    private String today;
+    private List<DiaryResponse> diaries;
+    private Map<LocalDate, List<DiaryCheckLog>> groupedData;
 
     public static DiaryCheckListResponse of(String today, List<Diary> diaries, Map<LocalDate, List<DiaryCheckLog>> groupedData) {
-        return new DiaryCheckListResponse(today, diaries, groupedData);
+        return DiaryCheckListResponse.builder()
+                .today(today)
+                .diaries(diaries.stream().map(diary -> DiaryResponse.of(diary, diary.getMember(),today)).toList())
+                .groupedData(groupedData)
+                .build();
     }
 }
--- src/main/java/com/mypill/domain/diary/dto/response/DiaryResponse.java
+++ src/main/java/com/mypill/domain/diary/dto/response/DiaryResponse.java
@@ -1,5 +1,6 @@
 package com.mypill.domain.diary.dto.response;
 
+import com.mypill.domain.diary.entity.Diary;
 import com.mypill.domain.member.entity.Member;
 import lombok.Builder;
 import lombok.Data;
@@ -9,7 +10,29 @@ import java.time.LocalTime;
 @Data
 @Builder
 public class DiaryResponse {
-    private Member member;
+    private Long memberId;
     private String name;
     private LocalTime time;
+    private Long diaryId;
+    private boolean isCheckedWhen;
+
+    public static DiaryResponse of(Diary diary, Member member, String checkDate) {
+        return DiaryResponse.builder()
+                .memberId(member.getId())
+                .diaryId(diary.getId())
+                .name(diary.getName())
+                .time(diary.getTime())
+                .isCheckedWhen(diary.getTimeChecks().stream()
+                        .anyMatch(e -> e.getCheckDate().toString().equals(checkDate)))
+                .build();
+    }
+
+    public static DiaryResponse of(Diary diary, Member member) {
+        return DiaryResponse.builder()
+                .memberId(member.getId())
+                .diaryId(diary.getId())
+                .name(diary.getName())
+                .time(diary.getTime())
+                .build();
+    }
 }
